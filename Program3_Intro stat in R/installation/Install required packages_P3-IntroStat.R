ab96d780d3de286696fa58c363250f1b21fe32f1
Meena Choi <mnchoi67@gmail.com>
2017-04-26 11:23:02 -0400

    P3-instllation

--- Program3_Intro stat in R/installation/Install required packages_P3-IntroStat.R
+++ Program3_Intro stat in R/installation/Install required packages_P3-IntroStat.R
@@ -1,21 +1,7 @@
 ########################################################################################
 ## Install required packages for Advanced R course section from CRAN
-required_packages_CRAN <- c("ggplot2", "reshape2", "psych", "pwr")
+packages <- c("ggplot2", "reshape2", "psych", "pwr", "MSstats", "limma", "MSnbase", "isobar", "aLFQ", "MSnID", "RforProteomics")
 
-install.packages(required_packages_CRAN)
-
-########################################################################################
-## Tests whether all required packages 
-## Press the 'Source' button in the top right corner of this pane and check 
-## whether the output in the Console pane confirms that all packages are installed
-
-required_packages <- c(required_packages_CRAN, required_packages_BioC)
-
-installed_packages <- required_packages %in% installed.packages()[,"Package"]
-missing_packages <- required_packages[!installed_packages]
-if ( length(missing_packages) > 0 ) {
-  cat(sprintf('FOLLOWING PACKAGES NEED TO BE INSTALLED STILL:\n\t%s\n',
-              paste(missing_packages, collapse=', ')))
-} else{
-  cat('ALL PACKAGES ARE INSTALLED, WE\'RE GOOD TO GO!\n')
-}
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite(packages)
